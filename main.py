a1eefedcabab2e82af948362271a21b4a8b89b56
Guy Sheffer <guysoft@gmail.com>
2019-01-17 18:06:47 +0200

    Fix clock calls

--- main.py
+++ main.py
@@ -14,41 +14,31 @@ class MyApp(App):
     def second_thread(self, data):
         print("starting second thread")
         permission_status = check_permission(Permission.WRITE_EXTERNAL_STORAGE)
-        while permission_status is None or not permission_status:
-            print("permission status:")
-            print(status)
-            time.sleep(1)
+        print(permission_status)
         
-        print("should have permission")
+        if permission_status is not None and permission_status:
+            print("got permission")
+            path = os.environ["SECONDARY_STORAGE"]
+            test_path = os.path.join(path, "test_yay")
+            os.makedirs(test_path)
+        else:
+            Clock.schedule_once(self.second_thread, 1)
 
         
-        uri = os.environ["SECONDARY_STORAGE"]
-        test_path = os.path.join(uri, "test_yay")
-        os.makedirs(test_path)
+
             
 
     def callback(self, data):
         print("Pushed button, running")
-        """
-        if not check_permission(Permission.WRITE_EXTERNAL_STORAGE):
-            print("granding permission")
-            print(request_permission(Permission.WRITE_EXTERNAL_STORAGE))
-        else:
-            print("claims to have, trying anyway")
-            print(request_permission(Permission.WRITE_EXTERNAL_STORAGE))
-        """
         print("request permission")
         print(request_permission(Permission.WRITE_EXTERNAL_STORAGE))
         Clock.schedule_once(self.second_thread, 5)
-        
-
-        
 
 
 
     def build(self):
 
-        return Button(text='Tuch to test writing to ' + os.environ["SECONDARY_STORAGE"], on_press=self.callback)
+        return Button(text='Touch to test writing to ' + os.environ["SECONDARY_STORAGE"], on_press=self.callback)
 
 
 if __name__ == '__main__':
